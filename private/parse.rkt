9144aa8c70d1f291023bb44e1bcbacfad6808db0
HaoZhang <haozhang@antigen.top>
2026-03-17 22:04:10 +0800

    Close the connection when a udp association is sent to the server

--- private/parse.rkt
+++ private/parse.rkt
@@ -8,7 +8,7 @@
         (define cmd
             (case (read-byte in)
                 ((1) 'connect)
-                ((3) 'udp-associate)
+                ((3) #;'udp-associate (exit))
                 (else (exit))))
         (define dest-type
             (case (read-byte in)
