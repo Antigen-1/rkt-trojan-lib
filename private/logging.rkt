086eb6b2f6d06c0ff0441a307d857c4c0f24972d
HaoZhang <haozhang@antigen.top>
2026-03-20 19:40:35 +0800

    Fix `report`

--- private/logging.rkt
+++ private/logging.rkt
@@ -10,11 +10,14 @@
     (parameterize ((date-display-format 'chinese))
         (format "~a ~a(~a):\n~a" name level (date->string (current-date)) msg)))
 (define (report name level message)
-    (log-message trojan-logger level (if (symbol? name) name (string->symbol name)) (format-message name level message)))
+    (log-message trojan-logger level (if (symbol? name) name (string->symbol name)) (format-message name level message) #f #f))
 
 (module+ test
     (require rackunit racket/match)
     (define recver (make-log-receiver trojan-logger 'info))
     (report 'Test 'info "test-logging")
+    (check-match (sync recver)
+        (vector 'info (and (regexp "Test") (regexp "test-logging")) #f 'Test))
+    (report "Test" 'info "test-logging")
     (check-match (sync recver)
         (vector 'info (and (regexp "Test") (regexp "test-logging")) #f 'Test)))
