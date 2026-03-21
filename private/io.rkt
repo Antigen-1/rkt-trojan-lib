896c3a2c8d82cb07d5430d0db645fc7a343070ad
HaoZhang <haozhang@antigen.top>
2026-03-21 16:00:01 +0800

    Fix `opt:copy-port`

--- private/io.rkt
+++ private/io.rkt
@@ -1,21 +1,28 @@
-#lang racket/base
-(require racket/port racket/contract)
-(provide (contract-out (opt:copy-port (->* (input-port? output-port?) (exact-positive-integer?) any))))
+#lang typed/racket
+(provide opt:copy-port)
 
-(define (opt:copy-port in out (size 4096))
+(define (opt:copy-port (in : Input-Port) (out : Output-Port) (size : Exact-Positive-Integer 4096)) : Void
     (define s (make-bytes size))
-    (let loop ()
+    (let read-loop ()
         (define e/n (read-bytes-avail! s in))
         (if (number? e/n)
-            (begin (write-bytes s out 0 e/n) (loop))
+            (let write-loop ((wn : Natural 0))
+                (if (= wn e/n)
+                    (read-loop)
+                    (let ((an (write-bytes-avail s out wn e/n)))
+                        (write-loop (+ wn an)))))
             ;; Special values will be ignored.
             (void))))
 
-(module+ test
-    (require rackunit)
-    (define bstr (list->bytes (build-list 10000 (lambda (_) (random 0 255)))))
-    (check-equal?
-        bstr
-        (call-with-output-bytes
-            (lambda (out)
-                (opt:copy-port (open-input-bytes bstr) out)))))
+(module* test racket/base
+    (require rackunit racket/port (submod ".."))
+    (let loop ((n 10))
+        (cond ((= n 0) (void))
+              (else
+                (define bstr (list->bytes (build-list (random 2000 20000) (lambda (_) (random 0 255)))))
+                (check-equal?
+                    bstr
+                    (call-with-output-bytes
+                        (lambda (out)
+                            (opt:copy-port (open-input-bytes bstr) out))))
+                (loop (- n 1))))))
